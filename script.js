b6b5eeaf3a61a67e83117010c02f1e24628e45bb
damond-dev <traviesoanthony021@gmail.com>
2026-05-17 19:36:36 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
